1415b54975630d617492bd7b6fca01ba518a6602
Ruthmy <ruthmy17@gmail.com>
2024-06-09 18:21:01 -0600

    Created 'App.svelte' - Svelte's state-based user interface component.

---
+++ src/components/App.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { Alert } from "flowbite-svelte";
+  import { APP_STATUS, appStatus } from "../store.ts";
+  import StepLoading from "./StepLoading.svelte";
+  import StepUpload from "./StepUpload.svelte";
+  import StepChat from "./StepChat.svelte";
+</script>
+
+{#if $appStatus === APP_STATUS.INIT}
+  <StepUpload />
+{:else if $appStatus === APP_STATUS.LOADING}
+  <StepLoading />
+{:else if $appStatus === APP_STATUS.ERROR}
+  <Alert>
+    <span class="font-medium">Something happend!</span>
+    Change a few things up and try submitting again.
+  </Alert>
+{:else if $appStatus === APP_STATUS.CHAT_MODE}
+  <p>Chat mode activate!</p>
+  <StepChat />
+{:else}
+  <Alert>
+    <span class="font-medium">Error!</span>
+    This state is not handled.
+  </Alert>
+{/if}
