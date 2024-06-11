a737be8d588906aca81942da2274409c1391cec0
Ruthmy <ruthmy17@gmail.com>
2024-06-11 14:01:47 -0600

    Created 'StepLoading.svelte' - Loading view with a spinner component.

---
+++ src/components/StepLoading.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { Spinner } from "flowbite-svelte";
+</script>
+
+<div class="flex justify-center items-center flex-col gap-y-2">
+  <Spinner color="blue" />
+  <p class="opacity-70">Loading file and extracting text...</p>
+</div>
