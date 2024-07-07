e5a5b941e665606cb3150a0271be80369227619d
Ruthmy <ruthmy17@gmail.com>
2024-07-07 13:09:42 -0600

    Modified 'upload.ts' -api- - Added the ocr to extract data from the upload.

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -1,4 +1,6 @@
 import type { APIRoute } from "astro";
+import fs from 'node:fs/promises';
+import path from 'node:path';
 
 import { v2 as cloudinary, type UploadApiResponse } from 'cloudinary';
 
@@ -9,9 +11,12 @@ cloudinary.config({
   api_secret: import.meta.env.CLOUDINARY_API_SECRET
 });
 
+const outputDir = path.join(process.cwd(), 'public/text');
+
 // Upload data stream to Cloudinary this way nothing is paralyzed with large files
 const uploadStream = async (buffer: Uint8Array, options: {
   folder: string,
+  ocr?: string,
 }): Promise<UploadApiResponse> => {
   return new Promise((resolve, reject) => {
     cloudinary
@@ -20,6 +25,10 @@ const uploadStream = async (buffer: Uint8Array, options: {
         if (result) return resolve(result);
         reject(error);
       }).end(buffer);
+  }).catch((error) => {
+    // show cloudinary error
+    console.error('Error message:', error.message);
+    throw new Error('Failed to upload file');
   });
 }
 
@@ -35,15 +44,34 @@ export const POST: APIRoute = async ({ request }) => {
   const arrayBuffer = await file.arrayBuffer();
   const uint8Array = new Uint8Array(arrayBuffer);
 
+  // Upload the file to Cloudinary
   const result = await uploadStream(uint8Array, {
-    folder: 'pdf'
+    folder: 'pdf',
+    ocr: 'adv_ocr'
   });
 
   const {
     asset_id: id,
     secure_url: url,
-    pages
+    pages,
+    info
   } = result;
+
+  // Extract OCR data from the upload
+  const data = info?.ocr?.adv_ocr?.data;
+
+  // Extract the text from the OCR data
+  const text = data.map((blocks: { textAnnotations: { description: string }[] }) => {
+    const annotations = blocks['textAnnotations'] ?? {};
+    const first = annotations[0] ?? {};
+    const content = first['description'] ?? '';
+    return content.trim();
+  }).filter(Boolean).join('\n')
+
+// TODO: Meter esta info en una base de datos
+// Mejor todavía en un vector y hacer los embeddings
+
+fs.writeFile(`${outputDir}/${id}.txt`, text, 'utf-8');
 
   return new Response(JSON.stringify({
     id,
