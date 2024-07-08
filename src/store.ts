13c2096c9042f5f16e91be8a2005f1be13529fb9
Ruthmy <ruthmy17@gmail.com>
2024-07-08 16:43:26 -0600

    Modified 'store.ts' -store- - Added a new status for insufficient quota.

--- src/store.ts
+++ src/store.ts
@@ -4,11 +4,16 @@ export const APP_STATUS = {
   INIT: 0,
   LOADING: 1,
   CHAT_MODE: 2,
-  ERROR: -1
+  ERROR: -1,
+  QUOTA: -2
 };
 
 export const appStatus = writable(APP_STATUS.INIT);
 export const appStatusInfo = writable({ id: '', url: '', pages: 0 });
+// export const appStatusInfo = writable({ 
+//   id: 'ed3a2e03d5027f316f701d6cd0e568ee', 
+//   url: 'https://res.cloudinary.com/drskp38ux/image/upload/v1720398959/pdf/v3i2yqb0zq5fwisyoqd4.pdf', 
+//   pages: 1 });
 
 export const setAppStatusLoading = () => {
   appStatus.set(APP_STATUS.LOADING);
@@ -16,6 +21,10 @@ export const setAppStatusLoading = () => {
 
 export const setAppStatusError = () => {
   appStatus.set(APP_STATUS.ERROR);
+}
+
+export const setAppStatusQuota = () => {
+  appStatus.set(APP_STATUS.QUOTA);
 }
 
 export const setAppStatusChatMode = (
